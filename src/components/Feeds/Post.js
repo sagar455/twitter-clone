31985e63aacb30ef96df31f4fe722d32aec0eb52
sagar455 <sagarkr4560@gmail.com>
2022-12-06 23:39:02 +0530

    feat: added tweet functionality

--- src/components/Feeds/Post.js
+++ src/components/Feeds/Post.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Avatar } from '@mui/material'
+import React, { forwardRef } from 'react';
+import { Avatar } from '@mui/material';
 import VerifiedIcon from '@mui/icons-material/Verified';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import RepeatIcon from '@mui/icons-material/Repeat';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import PublishIcon from '@mui/icons-material/Publish';
-import "./Post.css"
+import "./Post.css";
 
-function Post({displayName,username,verified,text,image,avatar}) {
+const Post =forwardRef(({displayName,username,verified,text,image,avatar}, ref) => {
   return (
-    <div className='post'>
+    <div className='post' ref={ref} >
       <div className="post_avatar">
         <Avatar src={avatar}></Avatar>
       </div>
@@ -39,6 +39,6 @@ function Post({displayName,username,verified,text,image,avatar}) {
     
      
   )
-}
+})
 
 export default Post
