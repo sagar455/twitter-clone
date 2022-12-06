b80ffe5e17142f09b011925fc1a61eeccd0c8557
sagar455 <sagarkr4560@gmail.com>
2022-12-06 17:24:08 +0530

    feat: widgets added

--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed} from 'react-twitter-embed';
+import SearchIcon from '@mui/icons-material/Search';
 import "./Widgets.css"
 
 function Widgets() {
   return (
-    <div className="widgets">Widgets</div>
+    <div className="widgets">
+      <div className="widgets_input">
+        <SearchIcon className='widgets_searchIcon'/>
+        <input type="text" placeholder='Search Twitter' />
+      </div>
+
+      <div className="widgets_widgetContainer">
+        <h2>What's happening</h2>
+        <TwitterTimelineEmbed
+            sourceType="profile"
+            screenName="ANI"
+            options={{height: 400}}/>
+
+          <TwitterTweetEmbed
+            tweetId={'1600093982931009536'}
+          />
+            <TwitterShareButton
+    url={'https://www.youtube.com/watch?v=Ke90Tje7VS0'}
+    options={{ text: '#reactjs is awesome', via: 'SagarKumar305' }}
+  />
+
+      </div>
+
+
+    </div>
   )
 }
 
